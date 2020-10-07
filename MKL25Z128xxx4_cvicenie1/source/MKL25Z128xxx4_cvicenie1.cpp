924e1bdaf9fdd3074718a0298f343d4dcde91fd2
Martin Hudik <hudikm@gmail.com>
2020-10-07 09:52:09 +0200

    Zmena konstanty

--- MKL25Z128xxx4_cvicenie1/source/MKL25Z128xxx4_cvicenie1.cpp
+++ MKL25Z128xxx4_cvicenie1/source/MKL25Z128xxx4_cvicenie1.cpp
@@ -57,7 +57,7 @@ int main(void) {
     BOARD_InitDebugConsole();
 #endif
 
-    PRINTF("Hello World %f \n", 0.05f);
+    PRINTF("Hello World %f \n", 0.10f);
 
     /* Force the counter to be placed into memory. */
     volatile static int i = 0 ;
